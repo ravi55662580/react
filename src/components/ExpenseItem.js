69fbcd82df9cf42fafd7df523f63430d4c36b34c
Your Name <you@example.com>
2024-01-19 13:59:41 +0530

    react lets start coding

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+    return (<>
+    <h2>Expense item!</h2>
+    <p>Food Rs 10</p>
+    <p>Petrol Rs 100</p>
+    <p>Movies Rs 200</p>
+    </>
+    )
+}
+
+export default ExpenseItem;
