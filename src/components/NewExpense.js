21e347eee7376fae156f25bb0b61dd1047adf8d4
Your Name <you@example.com>
2024-01-23 15:38:31 +0530

    DIY Challenges on conditional rendering

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
-  const saveExpenseDataHandler = (enteredExpenseData) => {
-    const expenseData = {
-      ...enteredExpenseData,
-      id: Math.random().toString()
+    const [isEditing, setIsEditing] = useState(false);
+  
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+      const expenseData = {
+        ...enteredExpenseData,
+        id: Math.random().toString(),
+      };
+      props.onAddExpense(expenseData);
+      setIsEditing(false);
     };
-    props.onAddExpense(expenseData);
+  
+    const startEditingHandler = () => {
+      setIsEditing(true);
+    };
+  
+    const stopEditingHandler = () => {
+      setIsEditing(false);
+    };
+  
+    return (
+      <div className='new-expense'>
+        {!isEditing && (
+          <button onClick={startEditingHandler}>Add New Expense</button>
+        )}
+        {isEditing && (
+          <ExpenseForm
+            onSaveExpenseData={saveExpenseDataHandler}
+            onCancel={stopEditingHandler}
+          />
+        )}
+      </div>
+    );
   };
-
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
-  );
-};
-
-export default NewExpense;
+  
+  export default NewExpense;
